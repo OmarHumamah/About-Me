2c517d781b30dbf0ea1a3a07efccd2cf8eb29e97
OmarHumamah <omar.nabeel.h@gmail.com>
2021-06-23 09:02:39 +0300

    lab02;;

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const _Name = prompt("Hello. what is your name" );
 alert("wellcome "+ _Name);
 console.log(_Name);
 let score=0;
+
 let _YorN1 = prompt('1. tell me one of my favorite colors. You have 6 attempts', 'red, green, blue, brown');
 console.log(_YorN1);
 let possibleAnswers = ['red', 'blue', 'orange', 'black',];
@@ -17,7 +18,8 @@ for (let i = 0; i < 6; i++) {
    
     console.log(i);
     console.log(_YorN1);
-}
+};
+
 alert('Here is all my favorite colors: '+ possibleAnswers[0]+',  '+possibleAnswers[3] );
 console.log(_YorN1);
 
@@ -37,9 +39,9 @@ for(let i=0; i<4 ; i++){
     }else
     _YorN2 = prompt('Try again');
     console.log(i);
-}
-
+};
 console.log(_YorN2);
+
 let _YorN3 = prompt('3. Do I like Hello catty TV show? ', '(Y)es or (N)o');
 switch(_YorN3.toLowerCase()){
     case 'yes':
@@ -49,10 +51,10 @@ switch(_YorN3.toLowerCase()){
     case 'no' :
     case 'n' :
         alert('Correct');
-        score++     
-         
-}
+        score++            
+};
 console.log(_YorN3);
+
  let _YorN4 = prompt('4. Do I like The waling dead series', '(Y)es or (N)o');
  switch(_YorN4.toLowerCase()){
     case 'yes':
@@ -62,10 +64,10 @@ console.log(_YorN3);
     break;
     case 'no' :
     case 'n' :
-        alert('Wrong');
-           
-}
+        alert('Wrong');           
+};
 console.log(_YorN4);
+
  let _YorN5 = prompt('5. Do i have an experience at phones and computers ', '(Y)es or (N)o');
  switch(_YorN5.toLowerCase()){
     case 'yes':
@@ -76,8 +78,9 @@ console.log(_YorN4);
     case 'no' :
     case 'n' :
         alert('Wrong'); 
-              }
+              };
 console.log(_YorN5);           
+
  let _YorN6 = prompt('6. Did i work at a factory', '(Y)es or (N)o');
  switch(_YorN6.toLowerCase()){
     case 'yes':
@@ -88,8 +91,9 @@ console.log(_YorN5);
     case 'n' :
         alert('Correct');
         score++;      
-}
+};
 console.log(_YorN6);
+
  let _YorN7 = prompt('7. Am I an Engineer?', '(Y)es or (N)o');
  switch(_YorN7.toLowerCase()){
     case 'yes':
@@ -100,6 +104,7 @@ console.log(_YorN6);
     case 'n' :
         alert('Correct'); 
         score++;      
-}
+};
 console.log(_YorN7);
+
 alert('Thank you '+ _Name + ', Your score is ' + score+ ' out of 7');
